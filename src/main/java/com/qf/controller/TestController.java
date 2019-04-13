ad83691d797c81082c12b7b6c1982fbdbcf907b8
xiaojie <13535590972@163.com>
2019-04-13 17:01:57 +0800

    as

--- src/main/java/com/qf/controller/TestController.java
+++ src/main/java/com/qf/controller/TestController.java
@@ -3,6 +3,7 @@ package com.qf.controller;
 public class TestController {
     public static void main(String[] args) {
         System.out.println("王松江");
+        System.out.println("Eririll");
     }
     /*请在controller包后，添加子包，子包加上自己模块的名字*/
 }
