9fa4247155c141652588ad45a90b70f30cbe38e3
zjxfox <359667676@qq.com>
2019-04-13 16:25:40 +0800

    zhangjinxiong

--- src/main/java/com/qf/controller/TestController.java
+++ src/main/java/com/qf/controller/TestController.java
@@ -2,7 +2,7 @@ package com.qf.controller;
 
 public class TestController {
     public static void main(String[] args) {
-
+        System.out.println("+++++++++++++");
     }
     /*请在controller包后，添加子包，子包加上自己模块的名字*/
 }
