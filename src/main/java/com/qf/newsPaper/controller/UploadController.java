bc3bfa3918968605b469569aa6a82783094f5bc4
MrAnt99 <xiedehui1995@Outlook.com>
2019-04-22 11:38:46 +0800

    富文本编辑器的功能提交

---
+++ src/main/java/com/qf/newsPaper/controller/UploadController.java
@@ -0,0 +1,64 @@
+package com.qf.newsPaper.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.servlet.http.HttpServletRequest;
+import java.io.File;
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
+@Controller
+public class UploadController {
+
+    static String UPLOAD_PATH = "/static/upload/";
+    @ResponseBody
+    @RequestMapping(value = "upload1", method = RequestMethod.POST)
+    public Map<String, Object> upload1(MultipartFile editorFile, HttpServletRequest request) {
+        Map<String, Object> result = new HashMap<String, Object>();
+
+        // 获取文件后缀
+        String fileName = editorFile.getOriginalFilename();
+        String fileSuffix = fileName.substring(fileName.lastIndexOf("."));
+
+
+        // 文件存放路径
+        String filePath = request.getSession().getServletContext().getRealPath(UPLOAD_PATH);
+        // 网络地址
+        InetAddress ia=null;
+        try {
+            ia = ia.getLocalHost();
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        }
+
+        // 判断路径是否存在，不存在则创建文件夹
+        File file = new File(filePath);
+        if (!file.exists()) {
+            file.mkdirs();
+        }
+
+        // 将文件写入目标
+        file = new File(filePath, UUID.randomUUID() + fileSuffix);
+        try {
+            editorFile.transferTo(file);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        // 获取服务端路径
+        String serverPath = String.format("%s://%s:%s%s%s", request.getScheme(), ia.getHostAddress(), request.getServerPort(), request.getContextPath(), UPLOAD_PATH);
+
+        // 返回给 wangEditor 的数据格式
+        result.put("errno", 0);
+        result.put("data", new String[]{serverPath + file.getName()});
+        return result;
+    }
+}
